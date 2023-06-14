dfcde4028a0688edbe042a526566abe91b6c21b5
Moises Jr <m.junior@eq.ufrj.br>
2023-06-14 12:52:50 -0300

    feat: internal server integrates with external api

---
+++ app/api/video/route.ts
@@ -0,0 +1,13 @@
+const ACCESS_KEY: string | undefined = process.env.ACCESS_KEY;
+
+export async function GET(query: string) {
+	query = "nextjs";
+
+	const res = await fetch(`https://api.pexels.com/videos/popular`, {
+		headers: { Authorization: `${process.env.ACCESS_KEY}` },
+	});
+
+	const data = await res.json();
+
+	return new Response(JSON.stringify(data), { status: 200 });
+}
