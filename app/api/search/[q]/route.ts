857bfe2760d3a3ee56fd2f98a4843b053c48451c
Moises Jr <m.junior@eq.ufrj.br>
2023-06-17 21:54:57 -0300

    feat: search route to filter videos and abstracts external API

--- app/api/search/[q]/route.ts
+++ app/api/search/[q]/route.ts
@@ -4,12 +4,9 @@ const ACCESS_KEY: string | undefined = process.env.ACCESS_KEY;
 const API_URL: string | undefined = process.env.API_URL;
 
 export async function GET(request: Request, { params }: VideoPageParams) {
-	const res = await fetch(
-		`${API_URL}/videos/${params.videoId}`,
-		{
-			headers: { Authorization: `${ACCESS_KEY}` },
-		}
-	);
+	const res = await fetch(`${API_URL}/search?query=${params.q}`, {
+		headers: { Authorization: `${ACCESS_KEY}` },
+	});
 
 	const data = await res.json();
 
--- app/api/video/route.ts
+++ app/api/video/route.ts
@@ -1,8 +1,9 @@
 const ACCESS_KEY: string | undefined = process.env.ACCESS_KEY;
+const API_URL: string | undefined = process.env.API_URL;
 
 export async function GET() {
-	const res = await fetch(`https://api.pexels.com/videos/popular`, {
-		headers: { Authorization: `${process.env.ACCESS_KEY}` },
+	const res = await fetch(`${API_URL}/popular`, {
+		headers: { Authorization: `${ACCESS_KEY}` },
 	});
 
 	const data = await res.json();
