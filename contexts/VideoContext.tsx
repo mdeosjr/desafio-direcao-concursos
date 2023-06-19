71efa720fc7fa631194b76b74885ac606948c83a
Moises Jr <m.junior@eq.ufrj.br>
2023-06-18 22:31:22 -0300

    refactor: changes default value from null to empty array

--- contexts/VideoContext.tsx
+++ contexts/VideoContext.tsx
@@ -1,11 +1,11 @@
-"use client"
+"use client";
 
 import { VideoData } from "@/types";
 import { createContext, useEffect, useState } from "react";
 
 interface VideosContextInterface {
-	videos: VideoData[] | null;
-	setVideos: React.Dispatch<React.SetStateAction<VideoData[] | null>>;
+	videos: VideoData[] | [];
+	setVideos: React.Dispatch<React.SetStateAction<VideoData[] | []>>;
 }
 
 interface Props {
@@ -15,24 +15,24 @@ interface Props {
 const VideosContext = createContext<VideosContextInterface | null>(null);
 
 export function VideosProvider({ children }: Props) {
-	const [videos, setVideos] = useState<VideoData[] | null>(null);
-	
+	const [videos, setVideos] = useState<VideoData[] | []>([]);
+
 	const fetchVideos = async () => {
 		const response = await fetch("/api/video");
 		const data = await response.json();
 
 		setVideos(data.videos);
 	};
-	
+
 	useEffect(() => {
 		fetchVideos();
-	}, [])
+	}, []);
 
 	return (
 		<VideosContext.Provider
 			value={{
 				videos,
-        setVideos
+				setVideos
 			}}
 		>
 			{children}
