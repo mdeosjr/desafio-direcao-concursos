730eaea2d4072255d9fbbc80acb18d45ccef01fa
Moises Jr <m.junior@eq.ufrj.br>
2023-06-18 22:32:11 -0300

    styling: new styling and add responsive media queries

--- components/Feed/index.tsx
+++ components/Feed/index.tsx
@@ -2,9 +2,15 @@ import { VideoData } from "@/types";
 import styles from "./feed.module.css";
 import { VideoCard } from "@/components";
 
-export default function Feed({ videos }: { videos: VideoData[] }) {
+export default function Feed({
+	videos,
+	className,
+}: {
+	videos: VideoData[];
+	className?: string;
+}) {
 	return (
-		<div className={styles.Grid}>
+		<div className={`${styles.Grid} ${className}`}>
 			{videos?.map((video: VideoData) => (
 				<VideoCard data={video} key={video.id} />
 			))}
--- components/index.ts
+++ components/index.ts
@@ -1,11 +1,15 @@
+"use client";
+
 import Video from "./Video";
 import Feed from "./Feed";
 import SideBar from "./SideBar";
 import VideoCard from "./VideoCard";
+import Header from "./Header";
 
 export {
   Video,
   Feed,
   SideBar,
-  VideoCard
+  VideoCard,
+  Header
 }
